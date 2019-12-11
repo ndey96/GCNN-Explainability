eee1e377f899f1e4956c06c01e26eb63cc74d923
Nolan Dey <ndey96@gmail.com>
2019-12-11 01:04:48 -0500

    Write training and testing code

--- gcn.py
+++ gcn.py
@@ -6,9 +6,9 @@ import torch.nn.functional as F
 from torch_geometric.datasets import Planetoid, TUDataset
 import torch_geometric.transforms as T
 from torch_geometric.nn import GCNConv, ChebConv, global_mean_pool
-from data import load_data
+from data import load_bbbp
 from torch_geometric.data import DataLoader
-
+import numpy as np
 # parser = argparse.ArgumentParser()
 # parser.add_argument('--use_gdc', action='store_true',
 #                     help='Use GDC preprocessing.')
@@ -19,13 +19,17 @@ H_0 = 8
 H_1 = 128
 H_2 = 256
 H_3 = 512
-
+train_frac = 0.8
 # dataset = TUDataset(root='/tmp/Tox21_AR', name='Tox21_AR')
 dataset = load_bbbp(N)
-loader = DataLoader(dataset, batch_size=32, shuffle=True)
+split_idx = int(np.floor(len(dataset)*train_frac))
+train = dataset[:split_idx]
+test = dataset[:split_idx]
+train_loader = DataLoader(train, batch_size=32, shuffle=True)
+test_loader = DataLoader(test, batch_size=32, shuffle=False)
 
 class Net(torch.nn.Module):
-    def __init__(self):
+    def __init__(self, H_0, H_1, H_2, H_3):
         super(Net, self).__init__()
         self.conv1 = GCNConv(H_0, H_1)
         self.conv2 = GCNConv(H_1, H_2)
@@ -41,36 +45,38 @@ class Net(torch.nn.Module):
 
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-model, data = Net().to(device), data.to(device)
-optimizer = torch.optim.Adam([
-    dict(params=model.reg_params, weight_decay=5e-4),
-    dict(params=model.non_reg_params, weight_decay=0)
-], lr=0.01)
+model = Net(H_0, H_1, H_2, H_3).to(device)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=20, gamma=0.5)
 
-
-def train():
+def train(loader):
     model.train()
-    optimizer.zero_grad()
-    F.nll_loss(model()[data.train_mask], data.y[data.train_mask]).backward()
-    optimizer.step()
+    total_loss = 0
+    for data in loader:
+        data = data.to(device)
+        optimizer.zero_grad()
+        out = model(data)
+        loss = F.binary_cross_entropy(out, data.y)
+        loss.backward()
+        total_loss += loss.item() * data.num_graphs
+        optimizer.step()
+    return total_loss / len(train)
 
 
-def test():
+def test(loader):
     model.eval()
-    logits, accs = model(), []
-    for _, mask in data('train_mask', 'val_mask', 'test_mask'):
-        pred = logits[mask].max(1)[1]
-        acc = pred.eq(data.y[mask]).sum().item() / mask.sum().item()
-        accs.append(acc)
-    return accs
+    correct = 0
+    for data in loader:
+        data = data.to(device)
+        with torch.no_grad():
+            pred = model(data).max(dim=1)[1]
+        correct += pred.eq(data.y).sum().item()
+    return correct / len(loader.dataset)
 
 
-best_val_acc = test_acc = 0
 for epoch in range(1, 201):
-    train()
-    train_acc, val_acc, tmp_test_acc = test()
-    if val_acc > best_val_acc:
-        best_val_acc = val_acc
-        test_acc = tmp_test_acc
-    log = 'Epoch: {:03d}, Train: {:.4f}, Val: {:.4f}, Test: {:.4f}'
-    print(log.format(epoch, train_acc, best_val_acc, test_acc))
+    loss = train(train_loader)
+    test_acc = test(test_loader)
+    print('Epoch {:03d}, Loss: {:.4f}, Test: {:.4f}'.format(
+        epoch, loss, test_acc))
+    scheduler.step()
